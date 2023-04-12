094b90f42dab0c21d2a83fb400d9302ba52c8f8c
shivrjpatil <shivrajp972@gmail.com>
2023-04-12 14:58:26 +0530

    Added polymorphism examples

---
+++ OopsConcepts274/Polymorphism.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcepts274
+{
+    internal class Polymorphism
+    {
+        public void Add(int a, int b)
+        {
+            int sum = a + b;
+            Console.WriteLine("the sum of two nummberc is =" + sum);
+        }
+
+        public void Add(int a, int b, int c)
+        {
+            int sum = a + b + c;
+            Console.WriteLine("the sum of three number is =" + sum);
+        }
+    }
+    internal class Methodoverriding
+    {
+        public virtual void Addition(int a, int b)
+        {
+            int sum = a + b;
+            Console.WriteLine("the adition function "+sum);
+        }
+    }
+    class example : Methodoverriding
+    {
+        public override void Addition(int a, int b)
+        {
+            int sub = a - b;
+            Console.WriteLine("the substraction fuction"+sub);
+        }
+    }
+}
--- OopsConcepts274/Program.cs
+++ OopsConcepts274/Program.cs
@@ -12,16 +12,16 @@ namespace OopsConcepts274
         static void Main(string[] args)
         {
             Console.WriteLine("Welcoke to oops concepts");
-            Console.WriteLine("1.Class and Object \n2.Inheritance");
+            Console.WriteLine("1.Class and Object \n2.Inheritance \n3.Polymorphism");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
-                 ClassandObject myObject = new ClassandObject();   
-                 Console.WriteLine(myObject.name);               
-                 Console.WriteLine(myObject.age);
+                    ClassandObject myObject = new ClassandObject();   
+                    Console.WriteLine(myObject.name);               
+                    Console.WriteLine(myObject.age);
                     break;
-                    case 2:
+                case 2:
                     Maruthi maruthi = new Maruthi();
                     maruthi.Model = "alto";
                     maruthi.color = "white";
@@ -29,7 +29,16 @@ namespace OopsConcepts274
                     maruthi.milage = 22;
                     maruthi.PrintCarInformation();
                     maruthi.PrintMilage();
-
+                    break;
+                case 3:
+                    
+                    Polymorphism methodoverloading=new Polymorphism();
+                    methodoverloading.Add(3,5);
+                    methodoverloading.Add(5,6,7);
+                    Methodoverriding methodoverriding = new Methodoverriding();
+                    methodoverriding.Addition(3,4);
+                    example example=new example();
+                    example.Addition(2,4);
                     break;
             }                                                            
             Console.ReadLine();                           
