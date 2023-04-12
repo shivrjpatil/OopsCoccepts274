41ad326f7e913707dc6c1c409a4c778a81458889
shivrjpatil <shivrajp972@gmail.com>
2023-04-12 07:31:09 +0530

    Added inheritance concept Example

---
+++ OopsConcepts274/Inheritance.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcepts274
+{
+    internal class Inheritance
+    {
+        public class car
+        {
+            public string Model;
+            public string color;
+            public int price;
+
+            public void PrintCarInformation()
+            {
+                Console.WriteLine("model:" + Model);
+                Console.WriteLine("Color:" + color);
+                Console.WriteLine("Price:" + price);
+            }
+        }
+
+        public class Maruthi : car
+        {
+            public int milage;
+            public void PrintMilage()
+            {
+                Console.WriteLine("Milage:" + milage);
+            }
+        }
+    }
+}
--- OopsConcepts274/Program.cs
+++ OopsConcepts274/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static OopsConcepts274.Inheritance;
 
 namespace OopsConcepts274
 {
@@ -11,10 +12,27 @@ namespace OopsConcepts274
         static void Main(string[] args)
         {
             Console.WriteLine("Welcoke to oops concepts");
-            ClassandObject myObject = new ClassandObject();  //This is the object creation 
-            Console.WriteLine(myObject.name);                //we are creating object for ClassandObject Class
-            Console.WriteLine(myObject.age);                //here object name is myObject with help of object                  
-            Console.ReadLine();                           //we are accessing the variables
+            Console.WriteLine("1.Class and Object \n2.Inheritance");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                 ClassandObject myObject = new ClassandObject();   
+                 Console.WriteLine(myObject.name);               
+                 Console.WriteLine(myObject.age);
+                    break;
+                    case 2:
+                    Maruthi maruthi = new Maruthi();
+                    maruthi.Model = "alto";
+                    maruthi.color = "white";
+                    maruthi.price = 200000;
+                    maruthi.milage = 22;
+                    maruthi.PrintCarInformation();
+                    maruthi.PrintMilage();
+
+                    break;
+            }                                                            
+            Console.ReadLine();                           
         }
     }
 }
